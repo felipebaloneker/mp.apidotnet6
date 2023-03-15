56b7ac819349719993a628d2236a7af4d0501b03
Felipe Baloneker <felipebaloneker23@gmail.com>
2023-03-14 23:20:02 -0300

    feature/ added maps

---
+++ MP.ApiDotNet6.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mp.ApiDotNet6.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP.ApiDotNet6.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Person> People { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
---
+++ MP.ApiDotNet6.Infra.Data/Maps/PersonMap.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Mp.ApiDotNet6.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP.ApiDotNet6.Infra.Data.Maps
+{
+    public class PersonMap : IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.ToTable("Pessoa");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .HasColumnName("IdPessoa")
+                .UseIdentityColumn();
+            builder.Property(x => x.Document)
+                .HasColumnName("Documento");
+            builder.Property(x => x.Name)
+                .HasColumnName("Nome");
+            builder.Property(x => x.Phone)
+                .HasColumnName("Celular");
+
+            builder.HasMany(x => x.Purchases)
+                .WithOne(p => p.Person)
+                .HasForeignKey(x => x.PersonId);
+
+        }
+    }
+}
